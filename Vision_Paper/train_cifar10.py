73683debe143916517c8b79e6f65e5e1a671de60
seosungwon <ssw9999@naver.com>
2022-04-20 21:48:15 +0900

    tutorial for AlexNet

---
+++ Vision_Paper/train_cifar10.py
@@ -0,0 +1,16 @@
+import torch.optim as optim
+import torch.utils.data
+import torch.backends.cudnn as cudnn
+import torchvision
+from torchvision import transforms as transforms
+import numpy as np
+
+import argparse
+from misc import progress_bar
+
+
+CLASSES = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+
+
